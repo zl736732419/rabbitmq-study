1af2320fd996cea031bdde867c18b0abf0cb6155
zhenglian <736732419@qq.com>
2018-04-26 09:58:22 +0800

    添加routing对消息进行分流

---
+++ src/test/java/com/zheng/rabbitmq/routing/LogReceiver1.java
@@ -0,0 +1,47 @@
+package com.zheng.rabbitmq.routing;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import com.zheng.rabbitmq.Constants;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 对于这种发布订阅模型，需要消费者首先注册，目前创建的是非持久化的订阅
+ * @Author zhenglian
+ * @Date 2018/4/25 23:53
+ */
+public class LogReceiver1 {
+    private static final String ROUTING_KEY = "error";
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(Constants.HOST);
+        factory.setPort(Constants.PORT);
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.exchangeDeclare(Constants.EXCHANGE_NAME, "direct");
+        // 这里没有指定queueName，由broker提供channel范围内唯一的名称
+        String queueName = channel.queueDeclare().getQueue();
+        System.out.println("queue name: " + queueName);
+        // 将queue与exchange进行绑定，从而获取exchange中的内容
+        channel.queueBind(queueName, Constants.EXCHANGE_NAME, ROUTING_KEY);
+
+        System.out.println(" [*] 1 Waiting for messages. To exit press CTRL+C");
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, StandardCharsets.UTF_8);
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+        channel.basicConsume(queueName, true, consumer);
+    }
+}
--- src/test/java/com/zheng/rabbitmq/routing/LogSender.java
+++ src/test/java/com/zheng/rabbitmq/routing/LogSender.java
@@ -1,4 +1,4 @@
-package com.zheng.rabbitmq.pubsub.nonpersistent;
+package com.zheng.rabbitmq.routing;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
@@ -8,6 +8,8 @@ import com.zheng.rabbitmq.Constants;
 import java.nio.charset.StandardCharsets;
 
 public class LogSender {
+    // 通过routing key对消息进行分流，不同routing key的消息会被对应相同routing key的queue接受
+    private static final String ROUTING_KEY = "error";
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(Constants.HOST);
@@ -15,13 +17,14 @@ public class LogSender {
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
         // 生产者直接与exchange打交道，由exchange与queue进行消息通信
-        // 采用fan out exchange,消息将会广播到所有注册到该exchange上的queue中
-        channel.exchangeDeclare(Constants.EXCHANGE_NAME, "fanout");
+        // 采用direct exchange,发送的消息将会被设置与消息相同的routing key的队列所接受
+        channel.exchangeDeclare(Constants.EXCHANGE_NAME, "direct");
 
         String message = "publish subscribe message";
 
         // 对于sub/pub类型消息，只需设置exchange即可，消费者通过与exchange绑定从而获取exchange中的消息，所以这里的queueName(routingKey)为空
-        channel.basicPublish(Constants.EXCHANGE_NAME, "", null, message.getBytes(StandardCharsets.UTF_8));
+        channel.basicPublish(Constants.EXCHANGE_NAME, ROUTING_KEY, null, 
+                message.getBytes(StandardCharsets.UTF_8));
         System.out.println(" [x] Sent '" + message + "'");
 
         channel.close();
